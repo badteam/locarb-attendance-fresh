2730b79d270c83aa77757af9f0a3c1a7e42701f6
badteam <badteam1@hotmail.com>
2025-08-13 13:13:56 +0300

    Update firebase-messaging-sw.js

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,1 +1,14 @@
+/* Firebase Messaging SW (Web Push) */
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
+firebase.initializeApp({
+  apiKey: "AIzaSyDSKUx-6RtCuLiGcnMVko0vAKvL9ik7hSI",
+  authDomain: "locarb-attendance-v2.firebaseapp.com",
+  projectId: "locarb-attendance-v2",
+  storageBucket: "locarb-attendance-v2.firebasestorage.app",
+  messagingSenderId: "953944468274",
+  appId: "1:953944468274:web:319947e61b55f1341b452b"
+});
+
+const messaging = firebase.messaging();
