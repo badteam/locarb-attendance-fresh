0271ad5745940518a62f47fcbfc875680a1bf004
badteam <badteam1@hotmail.com>
2025-08-16 17:16:22 +0300

    Create employee_home_screen.dart

---
+++ lib/screens/employee_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+
+class EmployeeHomeScreen extends StatelessWidget {
+  const EmployeeHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBar(title: Text('Employee Home')),
+      drawer: MainDrawer(),
+      body: Center(child: Text('Employee home (check-in/out coming later)')),
+    );
+  }
+}
